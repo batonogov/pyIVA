fb0d9debe49193ebf9a94ac2c1029b3a1747c599
Fedor Batonogov <batonogov@icloud.com>
2022-08-30 16:03:30 +0300

    Улучшение парсинга

--- iva.py
+++ iva.py
@@ -6,8 +6,15 @@ import time
 driver = webdriver.Chrome()
 
 driver.get('https://vcs.domrf.ru/doc/api/rest.html')
-time.sleep(2)
+time.sleep(1)
 items = [i for i  in [c.text.split() for c in driver.find_elements(By.CLASS_NAME, 'sc-fznNvL')] if i]
-print(items)
+
+# [print(c) for c in items]
+
+with open('pyIVA/__init__.py', 'w') as f:
+    f.write("__version__ = '0.0.1'\n\n")
+    [f.write(f"class {c[0].title()}:\n    pass\n\n") for c in items if c[0].isupper()]
+
+buttons = [' '.join(c) for c in items if not c[0].isupper()]
 
 driver.quit()
--- pyIVA/__init__.py
+++ pyIVA/__init__.py
@@ -1,2 +1,14 @@
 __version__ = '0.0.1'
 
+class Common:
+    pass
+
+class Chats:
+    pass
+
+class Conferences:
+    pass
+
+class Users:
+    pass
+
