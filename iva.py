149732673b1b8d466e1413b4572c943738a72b47
ilmestorun <nerv_viking@mail.ru>
2022-08-31 23:28:43 +0300

    добавил прокликивание по каждому элекменту

--- iva.py
+++ iva.py
@@ -3,17 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path=r'C:\\Users\\nerv_\\Downloads\\chromedriver_win32\\chromedriver.exe')
 
 driver.get('https://vcs.domrf.ru/doc/api/rest.html')
 time.sleep(1)
 
-with open('pyIVA/__init__.py', 'w') as f:
-    f.write("__version__ = '0.0.1'\n\n")
-    [f.write(f"class {c.title()}:\n    pass\n\n") for c in [' '.join(c).replace(' ', '_') for c in [c.text.split() for c in driver.find_elements(By.TAG_NAME, 'h1')][1:]]]
+# with open('pyIVA/__init__.py', 'w') as f:
+#     f.write("__version__ = '0.0.1'\n\n")
+#     [f.write(f"class {c.title()}:\n    pass\n\n") for c in [' '.join(c).replace(' ', '_') for c in [c.text.split() for c in driver.find_elements(By.TAG_NAME, 'h1')][1:]]]
 
-cmd_def = [i.text.replace(' ', '_') for i in driver.find_elements(By.TAG_NAME, 'h2')]
-print(cmd_def)   
+# cmd_def = [i.text.replace(' ', '_') for i in driver.find_elements(By.TAG_NAME, 'h2')]
+# print(cmd_def)   
+
+button = [' '.join(c) for c in [c.text.split() for c in driver.find_elements(By.TAG_NAME, 'h1')][1:]]
+
+for i in driver.find_elements(By.TAG_NAME, 'h1'):
+    if i.text in button:
+        i.click()
+        time.sleep(2)
+        for c in driver.find_elements(By.CLASS_NAME, 'dbpfvr'):
+            print([' '.join(c) for c in [c.text.split()]])
+        print(i.text)
+
+
 
 
 driver.quit()
