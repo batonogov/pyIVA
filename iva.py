562825b613e89887f709369064143427207205b3
ilmestorun <nerv_viking@mail.ru>
2022-08-31 12:49:29 +0300

    Создание верных классов

--- iva.py
+++ iva.py
@@ -1,3 +1,4 @@
+from faulthandler import is_enabled
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -7,20 +8,16 @@ driver = webdriver.Chrome()
 
 driver.get('https://vcs.domrf.ru/doc/api/rest.html')
 time.sleep(1)
-items = [i for i  in [c.text.split() for c in driver.find_elements(By.CLASS_NAME, 'sc-fznNvL')] if i]
 
-# [print(c) for c in items]
+
+
+buttons = [' '.join(c).replace(' ', '_') for c in [c.text.split() for c in driver.find_elements(By.TAG_NAME, 'h1')][1:]]
+
 
 with open('pyIVA/__init__.py', 'w') as f:
     f.write("__version__ = '0.0.1'\n\n")
-    [f.write(f"class {c[0].title()}:\n    pass\n\n") for c in items if c[0].isupper()]
+    [f.write(f"class {c.title()}:\n    pass\n\n") for c in buttons]
 
-buttons = [' '.join(c) for c in items if not c[0].isupper()]
-
-for i in driver.find_elements(By.CLASS_NAME, 'sc-fznNvL'):
-    if i.text in buttons:
-        i.click()
-        time.sleep(1)
 
 
 driver.quit()
--- pyIVA/__init__.py
+++ pyIVA/__init__.py
@@ -1,11 +1,92 @@
 __version__ = '0.0.1'
 
-class Common:
+class Introduction:
     pass
 
-class Chats:
+class Authentication:
     pass
 
-class Conferences:
+class Requests:
+    pass
+
+class Responses:
+    pass
+
+class Chat:
+    pass
+
+class Chat_Call:
+    pass
+
+class Conference:
+    pass
+
+class Conference_Session:
+    pass
+
+class Conference_Media:
+    pass
+
+class Conference_Participants:
+    pass
+
+class Conference_Lobby:
+    pass
+
+class Conference_Documents:
+    pass
+
+class Conference_Chat:
+    pass
+
+class Whiteboard:
+    pass
+
+class Screenshare:
+    pass
+
+class Conference_Templates:
+    pass
+
+class Conference_Statistics:
+    pass
+
+class User_Session:
+    pass
+
+class Profile:
+    pass
+
+class Interlocutors:
+    pass
+
+class Contacts:
+    pass
+
+class File_Resources:
+    pass
+
+class System:
+    pass
+
+class Client_Applications:
+    pass
+
+class Events_Subscription:
+    pass
+
+class Chat_Events:
+    pass
+
+class Conference_Events:
+    pass
+
+class Active_Conference_Events:
+    pass
+
+class Profile_Events:
+    pass
+
+class Contact_Events:
     pass
 
