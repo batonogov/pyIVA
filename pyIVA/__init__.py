4d2cde69cd82d6a2c412b33e2a01ca7306929a70
Fedor Batonogov <batonogov@icloud.com>
2022-09-03 13:18:44 +0300

    Улучшена запись в файл

--- pyIVA/__init__.py
+++ pyIVA/__init__.py
@@ -13,4 +13,93 @@ class Responses:
     pass
 
 class Chat:
-    def
+    def add_participants():
+        pass
+
+    def turn_on_notifications():
+        pass
+
+    def clear_chat_history():
+        pass
+
+    def create_group_chat():
+        pass
+
+    def get_chat():
+        pass
+
+    def delete_chat():
+        pass
+
+    def update_chat():
+        pass
+
+    def edit_message():
+        pass
+
+    def turn_off_notifications():
+        pass
+
+    def forward_messages():
+        pass
+
+    def search_attachments():
+        pass
+
+    def get_messages():
+        pass
+
+    def get_messages_changes():
+        pass
+
+    def get_user_starred_messages():
+        pass
+
+    def get_muted_chats():
+        pass
+
+    def get_p2p_chat():
+        pass
+
+    def get_chats():
+        pass
+
+    def search_chats():
+        pass
+
+    def remove_message():
+        pass
+
+    def remove_message_attachment():
+        pass
+
+    def remove_messages():
+        pass
+
+    def remove_participants():
+        pass
+
+    def send_audio_message():
+        pass
+
+    def send_message():
+        pass
+
+    def turn_on/off_notifications_from_user():
+        pass
+
+    def star_messages():
+        pass
+
+    def notify_about_typing():
+        pass
+
+    def unstar_messages():
+        pass
+
+    def update_participant():
+        pass
+
+    def notify_about_read():
+        pass
+
