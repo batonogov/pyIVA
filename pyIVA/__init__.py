81831f8c3bbd13f4d688c1d89566e8c4442bc181
ilmestorun <nerv_viking@mail.ru>
2022-09-03 13:09:45 +0300

    добавил нижнее подчёркивание

--- pyIVA/__init__.py
+++ pyIVA/__init__.py
@@ -13,4 +13,93 @@ class Responses:
     pass
 
 class Chat:
-    def
+ def Add_Participants():
+    pass
+
+def Turn_On_Notifications():
+    pass
+
+def Clear_Chat_History():
+    pass
+
+def Create_Group_Chat():
+    pass
+
+def Get_Chat():
+    pass
+
+def Delete_Chat():
+    pass
+
+def Update_Chat():
+    pass
+
+def Edit_Message():
+    pass
+
+def Turn_Off_Notifications():
+    pass
+
+def Forward_Messages():
+    pass
+
+def Search_Attachments():
+    pass
+
+def Get_Messages():
+    pass
+
+def Get_Messages_Changes():
+    pass
+
+def Get_User_Starred_Messages():
+    pass
+
+def Get_Muted_Chats():
+    pass
+
+def Get_P2P_Chat():
+    pass
+
+def Get_Chats():
+    pass
+
+def Search_Chats():
+    pass
+
+def Remove_Message():
+    pass
+
+def Remove_Message_Attachment():
+    pass
+
+def Remove_Messages():
+    pass
+
+def Remove_Participants():
+    pass
+
+def Send_Audio_Message():
+    pass
+
+def Send_Message():
+    pass
+
+def Turn_On/Off_Notifications_From_User():
+    pass
+
+def Star_Messages():
+    pass
+
+def Notify_About_Typing():
+    pass
+
+def Unstar_Messages():
+    pass
+
+def Update_Participant():
+    pass
+
+def Notify_About_Read():
+    pass
+
